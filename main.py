d131993b9900a9faae0bbf9c4b730e191b6562a3
Patipat Sangthong <phati1839@gmail.com>
2025-12-23 14:23:33 +0700

    create class EV

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 class Car:
-    def __init__(self, model, brand):
+    def __init__(self, brand, model):
         self.model = model
         self.brand = brand
 
     def drive(self):
         print(f'{self.brand} , {self.model} is driving')
 
-    my_car =Car('Toyota', 'Vios')
-    my_car.drive
-       
+my_car = Car('Toyota', 'Vios')
+my_car.drive
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery):
+        super().__init__(brand, model)
+        self.battery = battery
+
+    def charge(self):
+        print(f"{self.battery} Battery is charging...")
+    
+my_ev = ElectricCar("Tesla", "Model 3", 75) 
+my_ev.drive()
+my_ev.charge()
